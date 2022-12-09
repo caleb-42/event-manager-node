14e42d131101dc08bb305138e2794bc541e63d39
Ewere Ebie <37642292+caleb-42@users.noreply.github.com>
2022-12-09 08:37:36 +0100

    Update db.sql

--- db.sql
+++ db.sql
@@ -70,16 +70,10 @@ INSERT INTO events (admin_id,name,description,"location",speakers,start_date,end
 	 (1,'Seminar','Meeting of great minds to discuss the future of the civilisation','Paris','[{"name":"Elon Musk","desc":"Iron man","pic":"/avatar.png"},{"name":"Steve jobs","desc":"Innovator","pic":"/avatar.png"}]','2022-12-21 00:00:00','2022-12-23 00:00:00','2022-12-05 20:43:16.27917'),
 	 (1,'Book club','Reading to broaden our minds','Winslow Library','[{"name":"Harry potter","desc":"Wizard","pic":"/avatar.png"},{"name":"Frodo Baggins","desc":"Hobbit","pic":"/avatar.png"}]','2023-01-04 00:00:00','2023-01-08 00:00:00','2022-12-05 20:52:49.278313');
 
-INSERT INTO public.event_types (name,created_at,admin_id) VALUES
-	 ('MeetUp','2022-12-04 07:41:02.346853',1),
-	 ('Leap','2022-12-04 07:41:02.346853',1),
-	 ('Recruiting Mission','2022-12-04 07:41:02.346853',1),
-	 ('Hackathon','2022-12-04 07:41:02.346853',1),
-	 ('Premium-only Webinar','2022-12-04 07:41:02.346853',1),
-	 ('Open Webinar','2022-12-04 07:41:02.346853',1),
+INSERT INTO event_types (name,created_at,admin_id) VALUES
 	 ('leisure','2022-12-04 21:19:57.200058',1);
 
-INSERT INTO public.events_event_types (event_id,event_type_id) VALUES
+INSERT INTO events_event_types (event_id,event_type_id) VALUES
 	 (1,5),
 	 (2,2),
 	 (2,6),
